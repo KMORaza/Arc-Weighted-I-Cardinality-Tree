e81af566ed418303faa64a683f8f480b32ef255f
Raza <141234596+KMORaza@users.noreply.github.com>
2025-03-23 19:12:06 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,104 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <glpk.h>
 
-#define MAX_VERTICES 10
-#define MAX_EDGES 20
+#define MAX_NODES 100
 
-typedef struct {
-    int u, v;
-    double weight;
-} Edge;
-
-int n = 5;  
-int m = 8;  
-Edge edges[MAX_EDGES] = {
-    {0, 1, 1.0}, {0, 2, 2.0}, {1, 2, 3.0}, {1, 3, 4.0},
-    {2, 3, 5.0}, {3, 4, 6.0}, {2, 4, 7.0}, {0, 4, 8.0}
-};
-int l = 4;  
-
-int find(int parent[], int i) {
-    if (parent[i] == i)
-        return i;
-    return find(parent, parent[i]);
-}
-
-void union_sets(int parent[], int rank[], int x, int y) {
-    int xroot = find(parent, x);
-    int yroot = find(parent, y);
-    if (rank[xroot] < rank[yroot]) {
-        parent[xroot] = yroot;
-    } else if (rank[xroot] > rank[yroot]) {
-        parent[yroot] = xroot;
-    } else {
-        parent[yroot] = xroot;
-        rank[xroot]++;
+void solve_ILP(int n, int m, int graph[MAX_NODES][MAX_NODES], int terminals[], int l) {
+    glp_prob *lp;
+    int i, j, k;
+    int edge_count = 0;
+    
+    lp = glp_create_prob();
+    glp_set_prob_name(lp, "Arc-weighted l-cardinality tree");
+    glp_set_obj_dir(lp, GLP_MIN);
+    
+    glp_add_cols(lp, m);
+    for (i = 1; i <= m; i++) {
+        glp_set_col_kind(lp, i, GLP_BV);
+        glp_set_obj_coef(lp, i, graph[i-1][i]);
     }
-}
-
-int is_valid_tree(int selected_edges[], int edge_count) {
-    int parent[MAX_VERTICES];
-    int rank[MAX_VERTICES];
-    for (int i = 0; i < n; i++) {
-        parent[i] = i;
-        rank[i] = 0;
+    
+    int *row_ind = malloc((m+1) * sizeof(int));
+    double *row_val = malloc((m+1) * sizeof(double));
+    
+    glp_add_rows(lp, 1);
+    glp_set_row_name(lp, 1, "cardinality");
+    glp_set_row_bnds(lp, 1, GLP_UP, 0, l);
+    
+    for (i = 1; i <= m; i++) {
+        row_ind[i] = i;
+        row_val[i] = 1.0;
     }
-
-    int edge_in_tree = 0;
-    for (int i = 0; i < m; i++) {
-        if (selected_edges[i]) {
-            int u = edges[i].u;
-            int v = edges[i].v;
-            int set_u = find(parent, u);
-            int set_v = find(parent, v);
-            if (set_u == set_v) {
-                return 0; 
-            }
-            union_sets(parent, rank, set_u, set_v);
-            edge_in_tree++;
+    
+    glp_set_mat_row(lp, 1, m, row_ind, row_val);
+    
+    glp_simplex(lp, NULL);
+    
+    printf("Optimal Solution:\n");
+    for (i = 1; i <= m; i++) {
+        if (glp_get_col_prim(lp, i) > 0.5) {
+            printf("Edge %d included in the tree\n", i);
         }
     }
 
-    return edge_in_tree == l; 
-}
-
-double calculate_weight(int selected_edges[]) {
-    double total_weight = 0.0;
-    for (int i = 0; i < m; i++) {
-        if (selected_edges[i]) {
-            total_weight += edges[i].weight;
-        }
-    }
-    return total_weight;
-}
-
-void solve() {
-    int best_solution[MAX_EDGES];
-    double best_weight = 1e9; 
-    int total_combinations = 1 << m; 
-    for (int comb = 0; comb < total_combinations; comb++) {
-        int selected_edges[MAX_EDGES];
-        for (int i = 0; i < m; i++) {
-            selected_edges[i] = (comb >> i) & 1;
-        }
-        if (is_valid_tree(selected_edges, l)) {
-            double weight = calculate_weight(selected_edges);
-            if (weight < best_weight) {
-                best_weight = weight;
-                memcpy(best_solution, selected_edges, sizeof(selected_edges));
-            }
-        }
-    }
-    printf("Best tree with weight %.2f:\n", best_weight);
-    for (int i = 0; i < m; i++) {
-        if (best_solution[i]) {
-            printf("Edge: (%d, %d) with weight %.2f\n", edges[i].u, edges[i].v, edges[i].weight);
-        }
-    }
+    free(row_ind);
+    free(row_val);
+    glp_delete_prob(lp);
 }
 
 int main() {
-    solve();
+    int n = 5;
+    int m = 7;
+    int graph[MAX_NODES][MAX_NODES] = {
+        {0, 2, 3, 0, 0},
+        {2, 0, 4, 5, 0},
+        {3, 4, 0, 6, 7},
+        {0, 5, 6, 0, 8},
+        {0, 0, 7, 8, 0}
+    };
+    
+    int terminals[] = {1, 3, 4};
+    int l = 4;
+    
+    solve_ILP(n, m, graph, terminals, l);
+    
     return 0;
 }
